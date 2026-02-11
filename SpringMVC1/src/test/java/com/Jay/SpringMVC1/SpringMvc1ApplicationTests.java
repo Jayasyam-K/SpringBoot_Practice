203bee88945901647fadf376c26eed7541bf422f
Jayasyam <121284762+Jayasyam-K@users.noreply.github.com>
2026-02-11 18:57:06 +0530

    Added SpringMVC1

---
+++ SpringMVC1/src/test/java/com/Jay/SpringMVC1/SpringMvc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Jay.SpringMVC1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
